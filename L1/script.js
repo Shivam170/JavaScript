f2be88c890ac4ea77e0647b0c39c863cbe62eaf2
Shivam170 <shivamsharma4723@gmail.com>
2021-11-21 01:07:36 +0530

    "Function"

---
+++ L1/script.js
@@ -0,0 +1,63 @@
+'use strict';
+
+function logger(){
+    console.log("My name is Jonas");
+}
+
+// calling / running / invoking function
+logger();
+
+
+
+// Function declaration and Function Expression
+
+// Function declaration
+function calcAge1(birthYear){
+    return 2037- birthYear;
+}
+
+const age1 = calcAge1(1991);
+
+// Anonymous Function : function without a name 
+// Function expression
+
+const calcAge2 = function (birthYear) {
+    return 2037- birthYear;  
+}
+
+const age2 = calcAge2(1991);
+
+// Arrow function (Function Expression)
+
+const calcAge3 = birthYear=>2037-birthYear;
+const age3 = calcAge3(1991);
+
+console.log(age1, age2, age3);
+
+const yearsUntilRetirement = (birthYear,firstName) =>{
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991,"Jonas"));
+
+
+
+// Function Calling Another Function
+
+function cutFruitPieces(fruit){
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges){
+    const applePieces =  cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice wiht ${applePieces} apples and ${orangePieces} oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2,3));
+
