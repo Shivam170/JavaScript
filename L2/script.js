00ff79f0935d1bb5a28d0af13586ea53885e1a09
Shivam170 <shivamsharma4723@gmail.com>
2021-11-21 20:37:13 +0530

    Guess Game

--- L2/script.js
+++ L2/script.js
@@ -12,10 +12,51 @@
 
 // Handling Click Events
 
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector(".again").addEventListener("click", function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+});
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
   if (!guess) {
+    //When there is no input
     document.querySelector(".message").textContent = "No Number!";
+  } else if (guess === secretNumber) {
+    //When player wins
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector(".message").textContent = "Correct Number!";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+  } else if (guess > secretNumber) {
+    //When guess is too high
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too High!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    //When guess is too low
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too Low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
