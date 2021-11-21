c0a8f840f43086f2dfacb49493d9786b4ddcfbf1
Shivam170 <shivamsharma4723@gmail.com>
2021-11-21 20:42:30 +0530

    Guess Game Complete!

--- L2/script.js
+++ L2/script.js
@@ -14,6 +14,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector(".again").addEventListener("click", function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
@@ -38,6 +39,10 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "Correct Number!";
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+    if (score > highscore) {
+      highscore = score;
+    }
+    document.querySelector(".highscore").textContent = highscore;
   } else if (guess > secretNumber) {
     //When guess is too high
     if (score > 1) {
