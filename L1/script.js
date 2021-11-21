83f83fe523af73d7743b8ddf0a97fb92841714b6
Shivam170 <shivamsharma4723@gmail.com>
2021-11-21 19:36:47 +0530

    Guess Game

--- L1/script.js
+++ L1/script.js
@@ -1,63 +1,58 @@
-'use strict';
+"use strict";
 
-function logger(){
-    console.log("My name is Jonas");
+function logger() {
+  console.log("My name is Jonas");
 }
 
 // calling / running / invoking function
 logger();
 
-
-
 // Function declaration and Function Expression
 
 // Function declaration
-function calcAge1(birthYear){
-    return 2037- birthYear;
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
 }
 
 const age1 = calcAge1(1991);
 
-// Anonymous Function : function without a name 
+// Anonymous Function : function without a name
 // Function expression
 
 const calcAge2 = function (birthYear) {
-    return 2037- birthYear;  
-}
+  return 2037 - birthYear;
+};
 
 const age2 = calcAge2(1991);
 
 // Arrow function (Function Expression)
 
-const calcAge3 = birthYear=>2037-birthYear;
+const calcAge3 = (birthYear) => 2037 - birthYear;
 const age3 = calcAge3(1991);
 
 console.log(age1, age2, age3);
 
-const yearsUntilRetirement = (birthYear,firstName) =>{
-    const age = 2037 - birthYear;
-    const retirement = 65 - age;
-    // return retirement;
-    return `${firstName} retires in ${retirement} years`;
-}
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
 
-console.log(yearsUntilRetirement(1991,"Jonas"));
-
-
+console.log(yearsUntilRetirement(1991, "Jonas"));
 
 // Function Calling Another Function
 
-function cutFruitPieces(fruit){
-    return fruit * 4;
+function cutFruitPieces(fruit) {
+  return fruit * 4;
 }
 
-function fruitProcessor(apples, oranges){
-    const applePieces =  cutFruitPieces(apples);
-    const orangePieces = cutFruitPieces(oranges);
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
 
-    const juice = `Juice wiht ${applePieces} apples and ${orangePieces} oranges.`;
-    return juice;
+  const juice = `Juice wiht ${applePieces} apples and ${orangePieces} oranges.`;
+  return juice;
 }
 
-console.log(fruitProcessor(2,3));
-
+console.log(fruitProcessor(2, 3));
