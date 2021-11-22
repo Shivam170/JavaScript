cbf423089665a5a5d5183a962f8ffab86880344c
Shivam170 <shivamsharma4723@gmail.com>
2021-11-23 01:57:51 +0530

    Object Destructuring

--- Modern Operators/script.js
+++ Modern Operators/script.js
@@ -5,66 +5,96 @@ const flights =
   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
 // Data needed for first part of the section
-// const restaurant = {
-//   name: "Classico Italiano",
-//   location: "Via Angelo Tavanti 23, Firenze, Italy",
-//   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-//   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-//   mainMenu: ["Pizza", "Pasta", "Risotto"],
-
-//   openingHours: {
-//     thu: {
-//       open: 12,
-//       close: 22,
-//     },
-//     fri: {
-//       open: 11,
-//       close: 23,
-//     },
-//     sat: {
-//       open: 0, // Open 24 hours
-//       close: 24,
-//     },
-//   },
-// };
-
 const restaurant = {
   name: "Classico Italiano",
   location: "Via Angelo Tavanti 23, Firenze, Italy",
   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
 
-// Destructuring
+restaurant.orderDelivery({
+  time: "22:30",
+  address: "Via del Sole, 21",
+  mainIndex: 2,
+  starterIndex: 2,
+});
 
-const arr = [2, 3, 4];
-const a = arr[0];
-const b = arr[1];
-const c = arr[2];
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
 
-const [x, y, z] = arr;
-console.log(arr);
-console.log(x, y, z);
+// Different variable name
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, tags, hours);
 
-const [first, , second] = restaurant.categories;
-console.log(first, second);
+// Default Value
+const { menu = [], starterMenu: starters = [] } = restaurant;
 
-// Swapping
-[second, first] = [first, second];
+// Mutating Variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
 
-// Receive 2 return values from a function
-const [starter, main] = restaurant.order(2, 0);
+// Nested Objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
 
-// Nested Destructuring
-const nested = [2, 4, [5, 6]];
-// const [i, ,j] = nested;
+// Destructuring Arrays
 
-const [i, , [j, k]] = nested;
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
 
-// Default Values
+// const [x, y, z] = arr;
+// console.log(arr);
+// console.log(x, y, z);
 
-const [p = 1, q = 1, r = 1] = [2, 5];
+// let [first, , second] = restaurant.categories;
+// console.log(first, second);
+
+// // Swapping
+// [second, first] = [first, second];
+
+// // Receive 2 return values from a function
+// const [starter, main] = restaurant.order(2, 0);
+
+// // Nested Destructuring
+// const nested = [2, 4, [5, 6]];
+// // const [i, ,j] = nested;
+
+// const [i, , [j, k]] = nested;
+
+// // Default Values
+
+// const [p = 1, q = 1, r = 1] = [2, 5];
