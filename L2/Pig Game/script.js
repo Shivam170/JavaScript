9118582acbabc32124fd4f29b6351aaeeff56ab6
Shivam170 <shivamsharma4723@gmail.com>
2021-11-22 15:24:27 +0530

    Pig Game

---
+++ L2/Pig Game/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// Selecting Elements
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1"); // Slightly Faster
+const current0El = document.getElementById("current--0");
+
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+// Initial Condition
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
+
+const scores = [0, 0];
+
+let currentScore = 0;
+let activePlayer = 0;
+
+// Rolling dice Functionality
+btnRoll.addEventListener("click", function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display Dice
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore = currentScore + dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // Switch to next player
+    activePlayer = activePlayer === 0 ? 1 : 0;
+  }
+});
